a575f9a9e473f1987780e6f248202ce148f86850
Evelynn Gresbach <egresbach1@my.wctc.edu>
2022-04-16 18:58:18 -0500

    Add project files.

---
+++ App1/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace App1
+{
+    public partial class MainPage : ContentPage
+    {
+        private int Count = 0; 
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {   
+            Count++;
+            TheLabel.Text = $"{The_Input.Text} clicked {Count} times"; 
+        }
+
+        void Calculate_BMI(object sender, System.EventArgs e)
+        {
+            double weight = Double.Parse(User_Weight.Text);
+            double height = Double.Parse(User_Height.Text);
+            double BMI = (weight * 703) / (height * height);
+            BMI_Text.Text = $"{The_Input.Text}'s BMI is {Math.Round(BMI, 2)}"; 
+        }
+    }
+}
